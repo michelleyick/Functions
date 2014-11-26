f75a5ea1e65ab42caf9750bbc6bc12f0804648ff
41213 <41213@PC004141.coll.lr.local>
2014-11-26 15:16:09 +0000

    Starter 1

--- Functions starter 1.py
+++ Functions starter 1.py
@@ -2,7 +2,7 @@
 #24-11-2014
 #Functions starter 1
 
-#calculate total pay
+#calculate basic pay
 def calculate_basic_pay(hours,rate):
     total = hours*rate
     return total
@@ -14,17 +14,35 @@ def calculate_overtime_pay(hours,rate):
     total = overtime_pay+basic_pay
     return total
 
+#calculate total pay
 def calculate_total_pay(hours,rate):
     if hours <= 40:
-        total = calculate_basic_pay(40,1)
-    elif hours > 40:
-        total = calculate_overtime_pay(41,1)
+        total = calculate_basic_pay(hours,rate)
+    else:
+        total = calculate_overtime_pay(hours,rate)
+    return total
+
+#inputting the hours and rate of pay
+def hours_and_rate():
+    hours = int(input("Enter the number of hours you work for:"))
+    rate = int(input("Enter your rate of pay:"))
+    return hours,rate
+
+#displaying the total pay
+def display_total_pay(total):
+    total = calculate_total_pay(hours,rate)
     return total
     
-        
-#main program
-total_pay = calculate_basic_pay(12,2)
+#calculate pay
+def calculate_pay():
+    total = display_total_pay(total)
+    return total
+hours_rate = hours_and_rate()
+total_pay = calculate_pay()
 print(total_pay)
 
-total_
+        
+
+
+
 
