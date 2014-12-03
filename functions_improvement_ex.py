405942e0e7df91d98e770ad5531a2ebee65bf14e
41213 <41213@PC004141.coll.lr.local>
2014-12-03 13:49:13 +0000

    .

---
+++ functions_improvement_ex.py
@@ -0,0 +1,23 @@
+# functions improvement exercise (functions class exercises Task 1).
+
+# times-table tester
+import random
+
+# main program
+print("Times-table tester")
+print()
+testTable = input("Which times-table do you want to be tested on? ")
+testTable = int(testTable)
+for questions in range(1,6):
+    Num1 = testTable
+    Num2 = random.randrange(2,13)
+    Ans = Num1 * Num2
+    UserAnswer = input(str(Num1) + ' x ' + str(Num2) + ' = ? ')
+    UserAnswer = int(UserAnswer)
+    if UserAnswer == Ans:
+        print('Well done, you got the correct answer!')
+        print()
+    else:
+        print('Sorry, you got the answer wrong. The correct answer is',Ans)
+        print()
+              
