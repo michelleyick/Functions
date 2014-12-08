a4e7a5378b23ea6592e7b820179bbfa255e223c4
41213 <41213@PC004141.coll.lr.local>
2014-12-08 10:28:50 +0000

    .

--- Class exercises (Revision Q2).py
+++ Class exercises (Revision Q2).py
@@ -1,17 +1,16 @@
 #Michelle Yick
-#07-12-2014
+#08-12-2014
 #Functions class exercises. Revision Q2
 
 #The construction for the pyramid
 def pyramid_base(number):
-    total = number
+    total = number()
     return total
 
-#Calculate answer(construction of the pyramid
-def pyramid_body(total):
-    if total is odd:
-        total = total - 2
-        return total
+#Calculate answer(construction of the pyramid)
+def pyramid_body(number):
+    body = number()-2
+    return body
 
 #Get number
 def number():
@@ -19,11 +18,14 @@ def number():
     return number
 
 #Display pyramid
-def display_pyramid(total):
-    print(number,total,end="")
+def display_pyramid(total,body):
+    print(total,body)
 
 #Main program
 def printing_a_pyramid():
-    
+    total = pyramid_base(number)
+    total = pyramid_body(number)
+    display_pyramid(total,body)
+printing_a_pyramid()
     
     
